53b03d1dfd23796bd40503b54ab8d721950c279f
Tony Messias <tonyzrp@gmail.com>
2020-06-28 23:17:00 -0300

    Use standalone kustomize because of env and envs mismatch

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ if kubectl get job/laravel-app-migrator -n docker4laravel; then
     kubectl delete job/laravel-app-migrator -n docker4laravel --wait=true;
 fi
 
-kubectl apply -k .
+kustomize build | kubectl apply -f -
 
 kubectl wait --for=condition=complete --timeout=30s job/laravel-app-migrator -n docker4laravel
